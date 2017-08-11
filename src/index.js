c4987b95c1540e81e8fc65e84fec2a93eb37e4d4
Maycon Vinicius <maycon.vinicius@uaiti.com.br>
2017-08-11 16:15:38 -0300

    Registrando eventos de touch para o material-ui.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
 import App from './js/App'
 
 const renderApp = (NextApp) => {
+  // Needed for onTouchTap
+  // http://stackoverflow.com/a/34015469/988941
+  injectTapEventPlugin()
+
   ReactDOM.render(
     <AppContainer>
       <NextApp />
