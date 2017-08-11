2fe9d9dfa1152a2d56284088565e08c709d191d5
Maycon Vinicius <maycon.vinicius@uaiti.com.br>
2017-08-11 13:41:14 -0300

    Adicionado eslint.

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,23 @@ module.exports = {
   ],
 
   module: {
-    loaders: [
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: /src/,
+        loader: 'standard-loader',
+        options: {
+          // Emit errors instead of warnings (default = false)
+          error: false,
+          // enable snazzy output (default = true)
+          snazzy: true,
+          // other config options to be passed through to standard e.g.
+          parser: 'babel-eslint'
+        }
+      },
+
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -45,8 +61,6 @@ module.exports = {
       //     }
       //   }
       // },
-
-
     // ]
   },
 
